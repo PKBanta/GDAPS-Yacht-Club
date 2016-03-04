1053c115120ae3b97f157f584eaff006ac8a43ef
Jared White (RIT Student) <jmw8974@ad.rit.edu>
2016-03-04 10:40:59 -0500

    Added Level and Room classes

---
+++ SemesterProject/Level.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace SemesterProject
+{
+    class Level
+    {
+        // Fields
+        private LinkedList<Room> rooms; // This level's sequential list of rooms
+
+
+        // Properties
+        /// <summary>
+        /// Get the Room object at the specified index of the level's room
+        /// sequence
+        /// </summary>
+        /// <param name="index">Index in room sequence</param>
+        /// <returns>Room at <paramref name="index"/> in level sequence
+        /// </returns>
+        public Room this[int index]
+        {
+            get { return rooms.ElementAt(index); }
+        }
+
+
+        // Constructors
+        public Level(List<Room> levelRooms)
+        {
+            foreach (Room r in levelRooms)
+            {
+                rooms.AddLast(r);
+            }
+        }
+
+
+        // Methods
+    }
+}
---
+++ SemesterProject/Room.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace SemesterProject
+{
+    class Room
+    {
+        // Fields
+
+
+        // Properties
+
+
+
+        // Constructors
+        public Room()
+        {
+
+        }
+
+
+        // Methods
+    }
+}
