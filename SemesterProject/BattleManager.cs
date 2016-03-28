e3ea84b42e7723aaec178d7ffb7321029be6e18a
Peter Banta <pkb6087@rit.edu>
2016-03-28 16:47:42 -0400

    I made skeleton versions of Enemy and and Ally classes so that the basic enemy AI in the BattleManager class can work. The enemies attack a random ally or player. BattleManager now accepts a List<Character> in it's declaration, this is subject to change.

--- SemesterProject/BattleManager.cs
+++ SemesterProject/BattleManager.cs
@@ -7,5 +7,37 @@ namespace SemesterProject
 {
     class BattleManager
     {
+        private List<Character> characters;
+        private List<Enemy> enemies;
+        private List<Character> allies;
+        private Random rand;
+
+        public BattleManager(List<Character> chars)
+        {
+            rand = new Random();
+            characters = chars;
+            for(int i = 0; i < characters.Count; i++)
+            {
+                if (characters[i] is Enemy)
+                {
+                    enemies.Add((Enemy)characters[i]);
+                }
+
+                if(characters[i] is Player || characters[i] is Ally)
+                {
+                    allies.Add(characters[i]);
+                }
+            }
+        }
+
+        public void enemyTurn()
+        {
+            for(int i = 0; i < enemies.Count; i++)
+            {
+                enemies[i].Attack(allies[rand.Next(allies.Count)]);
+            }
+
+        }
+
     }
 }
---
+++ SemesterProject/Enemy.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework.Input;
+
+
+namespace SemesterProject
+{
+    class Enemy : Character
+    {
+        public Enemy(int x, int y, int width, int height, int damage, int maxHealth, Texture2D tex)
+            : base(x,y,width,height,damage,maxHealth,tex)
+        {
+
+
+        }
+    }
+}
