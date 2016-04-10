005dbe2cd04db77ba20ad0e1ff78b2a19558e21b
Adam Duvin <amduvin@optonline.net>
2016-04-10 19:06:49 -0400

    Updated the Character class and made Ally and Enemy classes

--- SemesterProject/Enemy.cs
+++ SemesterProject/Enemy.cs
@@ -17,5 +17,23 @@ namespace SemesterProject
 
 
         }
+
+        /// <summary>
+        /// Chooses the character to attack base on the health of each party member
+        /// They will go after the character with the most health
+        /// </summary>
+        /// <param name="playerParty">List of the player and their allies</param>
+        /// <returns>The character to attack</returns>
+        public Character Target(List<Character> playerParty)
+        {
+            Character targ = playerParty[0];
+
+            for(int i = 0; i < playerParty.Count; i++)
+            {
+                if (playerParty[i].Health > targ.Health) targ = playerParty[i];
+            }
+
+            return targ;
+        }
     }
 }
